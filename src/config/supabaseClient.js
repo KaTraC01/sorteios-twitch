fd6647391d404bdf602c3f5e193a593c945eb800
KaTraC01 <leonardosantiagounesp@gmail.com>
2025-02-21 18:11:50 -0300

    Corrigindo api key 3

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-// Pegando as variáveis de ambiente corretamente para a Vercel
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+// Pegando as variáveis de ambiente da Vercel ou do `.env.local`
+const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL || process.env.NEXT_PUBLIC_SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
     console.error("Erro: SUPABASE_URL ou SUPABASE_ANON_KEY não estão definidas.");
