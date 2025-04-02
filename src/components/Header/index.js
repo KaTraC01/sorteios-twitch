17569126e4d566b9a75223350f952236f2cca256
KaTraC01 <leonardosantiagounesp@gmail.com>
2025-04-02 18:21:21 -0300

    acertando os botoes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,19 @@
 ﻿import React from "react";
 import "./Header.css"; // Importando o CSS do Header
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Header() {
+    // Usando o hook useLocation para obter a rota atual
+    const location = useLocation();
+    const pathname = location.pathname;
+    
     return (
         <header className="header">
             <div className="logo">
-                <Link to="/" className="sorteio-link">🎲 Sorteio</Link>
+                <Link to="/" className={`sorteio-link ${pathname === '/' ? 'active' : ''}`}>🎲 Sorteio</Link>
             </div>
             <nav className="menu">
-                <Link to="/ganhadores">Ganhadores</Link>
+                <Link to="/ganhadores" className={pathname === '/ganhadores' ? 'active' : ''}>Ganhadores</Link>
             </nav>
         </header>
     );
